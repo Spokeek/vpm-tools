a8fcb9f02331e5a998c77ac24ec09923c47b551d
Spokeek <spokeek@protonmail.com>
2024-09-29 22:49:32 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Cocona;
+using VRC.PackageManagement.Core.Types.Packages;
+using VRC.PackageManagement.Core.Types.Providers;
+
+var builder = CoconaApp.CreateBuilder();
+var app = builder.Build();
+
+app.AddCommand((string repoListPath) =>
+{
+VRCRepoList repoList = new VRCRepoList();
+repoList.author = "Spokeek VPM Packages";
+repoList.id = "fr.spokeek.vpm-packages";
+repoList.url = "https://spokeek.github.io/vpm-repository";
+
+repoList.AddPackage(
+);
+
+    repoList.Save(repoListPath);
+});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,8 @@
+# VPM Tools
+
+This project is a CLI tool to provide helping features based on the VPM standards for VRChat
+
+# Technologies
+
+- C# Dotnet
+- 
